e562e7459b36285d9ff02a2879bcd4ad12769916
bp9640ATT <ba0087036@techmahindra.com>
2015-11-24 18:47:15 +0530

    LogParser

---
+++ LogParser/app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var login = require('./routes/login');
+
+var app = express();
+
+// view engine setup
+app.set('port', process.env.PORT || 3000);
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+app.use(logger('dev'));
+app.use(bodyParser());
+//app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/login', login);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message : err.message,
+			error : err
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message : err.message,
+		error : {}
+	});
+});
+console.log("Hi");
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
